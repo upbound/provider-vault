321989b9db02d14abcc2593b838dd804bc0eb9d8
Markus Schweig <markus.schweig@upbound.io>
2023-04-24 18:13:39 -0700

    added config for resources

---
+++ config/oktaauthbackendgroup/config.go
@@ -0,0 +1,11 @@
+package DIR_oktaauthbackendgroup
+
+import "github.com/upbound/upjet/pkg/config"
+
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("vault_okta_auth_backend_group", func(r *config.Resource) {
+		// We need to override the default group that upjet generated for
+		// this resource, which would be "vault"
+	})
+}
---
+++ config/pkisecretbackendconfigurls/config.go
@@ -0,0 +1,11 @@
+package DIR_pkisecretbackendconfigurls
+
+import "github.com/upbound/upjet/pkg/config"
+
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("vault_pki_secret_backend_config_urls", func(r *config.Resource) {
+		// We need to override the default group that upjet generated for
+		// this resource, which would be "vault"
+	})
+}
