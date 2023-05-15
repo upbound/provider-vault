1fe449bc5bb5ee3511ee9c3fe7e05a8dbee7600f
Markus Schweig <markus.schweig@upbound.io>
2023-05-15 16:02:14 -0700

    adjusted kubeconfig permissions

--- cluster/test/setup.sh
+++ cluster/test/setup.sh
@@ -1,25 +1,48 @@
 #!/usr/bin/env bash
 set -aeuo pipefail
 
-echo "Running setup.sh"
+# setting up colors
+BLU='\033[0;104m'
+YLW='\033[0;33m'
+GRN='\033[0;32m'
+RED='\033[0;31m'
+NOC='\033[0m' # No Color
 
-echo "Creating cloud credential secret..."
+echo_info(){
+    printf "\n${BLU}%s${NOC}\n" "$1"
+}
+echo_step(){
+    printf "\n${BLU}>>>>>>> %s${NOC}\n" "$1"
+}
+echo_step_completed(){
+    printf "${GRN} [✔] %s${NOC}\n" "$1"
+}
+
+echo_info "Running setup.sh"
+
+SCRIPT_DIR=$( cd -- $( dirname -- "${BASH_SOURCE[0]}" ) &> /dev/null && pwd )
+KUBECONFIG_PATH="${SCRIPT_DIR}/../../kubeconfig"
+if [ -f "${KUBECONFIG_PATH}" ]; then
+    chmod 0600 ${KUBECONFIG_PATH}
+fi
+
+echo_info "Creating cloud credential secret..."
 ${KUBECTL} -n upbound-system create secret generic provider-secret --from-literal=credentials="{\"token\":\"${UPTEST_CLOUD_CREDENTIALS}\"}" --dry-run=client -o yaml | ${KUBECTL} apply -f -
-echo "Waiting until provider is healthy..."
+echo_info "Waiting until provider is healthy..."
 ${KUBECTL} wait provider.pkg --all --for condition=Healthy --timeout 5m
 
-echo "Waiting for all pods to come online..."
+echo_info "Waiting for all pods to come online..."
 ${KUBECTL} -n upbound-system wait --for=condition=Available deployment --all --timeout=5m
 
-echo "Creating vault namesapce"
+echo_info "Creating vault namespace"
 ${KUBECTL} create namespace vault --dry-run=client -o yaml | kubectl apply -f -
 
-echo "Adding Hashicorp repo"
+echo_info "Adding Hashicorp repo"
 helm repo add hashicorp https://helm.releases.hashicorp.com --force-update
 
-echo "Checking for HashiCorp Vault installation"
+echo_info "Checking for HashiCorp Vault installation"
 VAULT_DEPLOYMENT_STATUS=$(helm status vault -n vault|grep STATUS || true)
-echo "$VAULT_DEPLOYMENT_STATUS"
+echo_info "$VAULT_DEPLOYMENT_STATUS"
 if [[ "$VAULT_DEPLOYMENT_STATUS" == "STATUS: deployed" ]]; then
     echo "Uninstalling Hashicorp vault; need clean installation"
     helm uninstall vault -n vault --wait
@@ -27,15 +50,15 @@ fi
 
 helm install vault hashicorp/vault -n vault
 
-echo "Waiting for vault deployments"
+echo_info "Waiting for vault deployments"
 ${KUBECTL} -n vault wait --for=condition=Available deployment --all --timeout=5m
 
 VAULT_NOT_RUNNING=true
 while [[ "$VAULT_NOT_RUNNING" == "true" ]]; do
-    echo "Waiting for vault-0 pod to run"
+    echo_step "Waiting for vault-0 pod to run"
     sleep 5
     VAULT_STATUS=$(${KUBECTL} get pod -n vault vault-0 -o jsonpath='{.status}'|jq|grep phase|awk '{print $2}'|tr -d '"'|tr -d ',')
-    echo "---$VAULT_STATUS---"
+    echo_info "---$VAULT_STATUS---"
     if [[ "$VAULT_STATUS" == "Running" ]]; then
 	VAULT_NOT_RUNNING=false
     fi
@@ -44,20 +67,20 @@ done
 echo "Initializing vault and obtaining unseal keys"
 ${KUBECTL} exec -n vault --stdin vault-0 -- vault operator init -format=yaml > vault-auto-unseal-keys.yaml
 
-echo "vault-auto-unseal-keys.yaml"
+echo_info "vault-auto-unseal-keys.yaml"
 cat vault-auto-unseal-keys.yaml
 VAULT_ROOT_TOKEN=$(grep root_token vault-auto-unseal-keys.yaml |awk -F: '{print $2}'|sed 's/ //g')
 
-echo "Unsealing vault - part 1/3"
+echo_info "Unsealing vault - part 1/3"
 ${KUBECTL} exec -n vault --stdin vault-0 -- vault operator unseal -tls-skip-verify $(grep -A 5 unseal_keys_b64 vault-auto-unseal-keys.yaml |head -2|tail -1|sed 's/- //g')
 
-echo "Unsealing vault - part 2/3"
+echo_info "Unsealing vault - part 2/3"
 ${KUBECTL} exec -n vault --stdin vault-0 -- vault operator unseal -tls-skip-verify $(grep -A 5 unseal_keys_b64 vault-auto-unseal-keys.yaml |head -3|tail -1|sed 's/- //g')
 
-echo "Unsealing vault - part 3/3"
+echo_info "Unsealing vault - part 3/3"
 ${KUBECTL} exec -n vault --stdin vault-0 -- vault operator unseal -tls-skip-verify $(grep -A 5 unseal_keys_b64 vault-auto-unseal-keys.yaml |head -4|tail -1|sed 's/- //g')
 
-echo "Writing provider-vault secret"
+echo_info "Writing provider-vault secret"
 cat <<EOF | ${KUBECTL} apply -f -
 apiVersion: v1
 kind: Secret
@@ -73,7 +96,7 @@ stringData:
     }
 EOF
 
-echo "Applying providerconfig"
+echo_info "Applying providerconfig"
 VAULT_0_POD_IP=$(${KUBECTL} get pod vault-0 -n vault -o yaml|grep podIP:|awk '{print $2}')
 cat <<EOF | ${KUBECTL} apply -f -
 apiVersion: vault.upbound.io/v1beta1
@@ -105,5 +128,6 @@ EOF
 # More useful setup info
 # https://itnext.io/vault-cluster-with-auto-unseal-on-kubernetes-8e469f9cdcfd
 
-echo "Test setup complete"
-echo "Running upjet vault tests"
+echo_step "Note: local-dev cluster will remain after tests"
+echo_step_completed "Test setup complete"
+echo_step "Running upjet vault tests"
