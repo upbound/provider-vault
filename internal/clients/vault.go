217cdd8b61e5e5af06df7f01930fd00879ba85fd
Markus Schweig <markus.schweig@upbound.io>
2023-07-19 07:58:40 -0700

    updated annotation

--- internal/clients/vault.go
+++ internal/clients/vault.go
@@ -65,6 +65,7 @@ const (
 
 // TerraformSetupBuilder builds Terraform a terraform.SetupFn function which
 // returns Terraform provider setup configuration
+// nolint:gocyclo
 func TerraformSetupBuilder(version, providerSource, providerVersion string) terraform.SetupFn {
 	return func(ctx context.Context, client client.Client, mg resource.Managed) (terraform.Setup, error) {
 		ps := terraform.Setup{
