d5ad30e367ac25adec944de3066197676d895722
Tobias Schlaepfer <tobias.schlaepfer@4data.ch>
2024-05-27 14:31:05 +0200

    update config.go to enable group alias references
    Signed-off-by: Tobias Schlaepfer <tobias.schlaepfer@4data.ch>

--- config/vault/config.go
+++ config/vault/config.go
@@ -8,4 +8,23 @@ func Configure(p *config.Provider) {
 		r.Kind = "VaultNamespace"
 		r.ShortGroup = "vault"
 	})
+	p.AddResourceConfigurator("vault_identity_group_alias", func(r *config.Resource) {
+        r.Kind = "GroupAlias"
+        r.ShortGroup = "identity"
+        r.References["canonical_id"] = config.Reference{
+            Type: "Group",
+        }
+        r.References["mount_accessor"] = config.Reference{
+            Type: "github.com/upbound/provider-vault/apis/jwt/v1alpha1.AuthBackend",
+        }
+        r.References["mount_accessor"] = config.Reference{
+            Type: "github.com/upbound/provider-vault/apis/github/v1alpha1.AuthBackend",
+        }
+        r.References["mount_accessor"] = config.Reference{
+            Type: "github.com/upbound/provider-vault/apis/ldap/v1alpha1.AuthBackend",
+        }
+        r.References["mount_accessor"] = config.Reference{
+            Type: "github.com/upbound/provider-vault/apis/okta/v1alpha1.AuthBackend",
+        }
+    })
 }
